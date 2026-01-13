37ad3c902f7a3cd25d8b5b95bd8d9e5ad5d0b7a8
Tyerone <141754956+Pro5essorE@users.noreply.github.com>
2026-01-13 00:58:25 -0500

    added nosignal functionality
    Added start and stop buttons for camera feed control, updated layout and styling.

--- MROV-25-ROS-OPERATOR/gen_camera_widget.py
+++ MROV-25-ROS-OPERATOR/gen_camera_widget.py
@@ -2,47 +2,64 @@
 A Generic Camera Widget - For now used for just testing out camera stuff
 Author: Tyerone Chen
 Create Date: 11/16/2025
-Last Update: 12/2/2025
+Last Update: 1/12/2026
 """
 # imports
+import os
 import rclpy
 from rclpy.node import Node
 from rqt_gui_py.plugin import Plugin
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
-from python_qt_binding.QtWidgets import QLabel, QVBoxLayout, QWidget, QSizePolicy
+from python_qt_binding.QtWidgets import QLabel, QVBoxLayout, QWidget, QSizePolicy, QPushButton
 from python_qt_binding.QtCore import Signal, Slot 
 from python_qt_binding.QtGui import QImage, QPixmap
+from ament_index_python.packages import get_package_share_directory
 
 # Camera Widget Class
 class GenCameraWidget(QWidget):
     # Consts
     SUB_TO = '/rov/camera/image_raw'
+    TIMEOUT_DELAY = 2.5
     # Vars
     image_signal = Signal(QPixmap)
+    is_subbed = False
     # Constructor
     def __init__(self, node_instance):
         super().__init__()
+        self.REF_W = 4
+        self.REF_H = 3
         # Custom ROS2 Node Setup
         self.node = node_instance
         # Setup
-        self.label = QLabel("Waiting for Camera Data...")
+        # Labels
+        self.label = QLabel(self)
         self.label.setScaledContents(True)
-        self.label.setSizePolicy(
-            QSizePolicy.Policy.Expanding,
-            QSizePolicy.Policy.Expanding
-        )
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+        self.label.setMinimumSize(self.REF_W, self.REF_H)
+        self.label.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        self.set_no_signal()
+        # Buttons - do later
+        self.start_button = QPushButton()
+        self.start_button.clicked.connect(self.start_feed)
+        self.stop_button = QPushButton()
+        self.stop_button.clicked.connect(self.stop_feed)
+        #Layout
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
         # Style Shit - do later
         self.setStyleSheet("""
             QWidget {
-                border: 1px solid #454d55;
+                color: #ccc;
+                background: #141414;
+                border: 3px solid #282828;
                 border-radius: 5px;
                 padding: 5px;
                 margin: 5px;
+            }
+            QPushButton {
+
             }
             QLabel {
             }
@@ -51,7 +68,7 @@ class GenCameraWidget(QWidget):
         self.image_signal.connect(self.update_image_label)
         # Sub Conncetion
         self.bridge = CvBridge()
-        self.sub = self.node.create_subscription(Image, self.SUB_TO, self.callback, 10)
+        self.start_feed()
     # METHODSS
     # callback crap
     def callback(self, msg):
@@ -65,7 +82,35 @@ class GenCameraWidget(QWidget):
             self.image_signal.emit(pixmap)
         except Exception as ex:
             self.node.get_logger().error(f"ERROR in camera callback: {ex}")
+    # no signal thing
+    def set_no_signal(self):
+        pkg_share = get_package_share_directory('py_rov_gui')
+        img_path = os.path.join(pkg_share, 'resource', 'no_data.png')
+        pixmap = QPixmap(img_path)
+        if pixmap.isNull():
+            self.label.setText("NO SIGNAL - [IMAGE MISSING]")
+        else:
+            self.label.setPixmap(pixmap)
+    # resize overide
+    def resizeEvent(self, event):
+        scale = min((self.width() / self.REF_W), (self.height() / self.REF_H))
+        # apply scale
+        new_w = int(self.REF_W * scale)
+        new_h = int(self.REF_H * scale)
+        self.label.setGeometry((self.width() - new_w) // 2, (self.height() - new_h) // 2, new_w, new_h)
         pass
+    # Buttons / Feed Methods
+    def start_feed(self): 
+        if not self.is_subbed:
+            self.sub = self.node.create_subscription(Image, self.SUB_TO, self.callback, 10)
+            self.is_subbed = True
+            self.node.get_logger().info("Camera Feed Started...")
+    def stop_feed(self):
+        if self.is_subbed:
+            self.node.destroy_subscription(self.sub)
+            self.sub = None
+            self.is_subbed = False
+            self.node.get_logger().info("Camera Feed Stopped...")
     # Slots
     @Slot(QPixmap)
     def update_image_label(self, pixmap):
