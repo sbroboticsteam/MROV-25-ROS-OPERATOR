6d66ce95f8899c2a02440c8749643fe2386a6148
Tyerone <141754956+Pro5essorE@users.noreply.github.com>
2026-01-13 00:54:48 -0500

    added ctrl_widget entry point

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ module_name = 'mrov_25_ros_operator' #idk if this is the right name?
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='1.1.0',
     packages=[package_name],
     data_files=[
         ('share/' +package_name+ '/resource', glob('resource/*.xml')), 
@@ -16,7 +16,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='tyerone',
+    maintainer='tyerone chen',
     maintainer_email='tchen11898@gmail.com',
     description='Something Gui Something',
     license='Apache-2.0',
@@ -36,7 +36,8 @@ setup(
             'GenDataWidget = '+module_name+'.gen_data_widget:GenDataPlugin',
             'LeakSensorWidget = '+module_name+'.leak_sensor_widget.LeakSensorPlugin',
             'MotorDataWidget = '+module_name+'.motor_data_widget.MotorDataPlugin',
-            'SpeedDataWidget = '+module_name+'.speed_data_widget.SpeedDataPlugin'
+            'SpeedDataWidget = '+module_name+'.speed_data_widget.SpeedDataPlugin',
+            'ControllerWidget = '+module_name+'.controller_widget.ControllerPlugin'
         ],
     },
 )
