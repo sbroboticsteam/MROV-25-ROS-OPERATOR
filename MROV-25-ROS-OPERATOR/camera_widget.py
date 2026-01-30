3599fce24e6cd3bd338359c193b900f54431e5c5
Tyerone <141754956+Pro5essorE@users.noreply.github.com>
2026-01-29 22:41:26 -0500

    added multithreading & compressed image
    updated to now use multithreading w/parallel callback groups. Also the feed is now displaying compressed images, was being a dum dum beforeand didnt realize how bad displaying the raw image was for performance

--- MROV-25-ROS-OPERATOR/camera_widget.py
+++ MROV-25-ROS-OPERATOR/camera_widget.py
@@ -2,34 +2,39 @@
 A Camera Widget - Used for displaying multiple camera feedss
 Author: Tyerone Chen
 Create Date: 11/16/2025
-Last Update: 1/26/2026
+Last Update: 1/29/2026
 """
 # imports
 import os
-import rclpy
-from rclpy.node import Node
-from rqt_gui_py.plugin import Plugin
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import cv2
-from python_qt_binding.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QSizePolicy
+import threading
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rqt_gui_py.plugin import Plugin
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge
+from python_qt_binding.QtWidgets import QLabel, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
 from python_qt_binding.QtCore import Signal, Slot, Qt
 from python_qt_binding.QtGui import QIcon, QImage, QPixmap
 from ament_index_python.packages import get_package_share_directory
-
 # Main Camera Widget, holds 3 instances of the GenCamera Widget
 class CameraWidget(QWidget):
     # Consts
-    PKG_PATH = get_package_share_directory('py_rov_gui')
     def __init__(self, node_instance):
         super().__init__()
+        self.node = node_instance
         # background color setup
         self.setAttribute(Qt.WA_StyledBackground, True)
         self.setStyleSheet('background-color: #141414;')
+        # setup multithreading
+        self.executor = MultiThreadedExecutor()
+        self.executor.add_node(self.node)
+        self.executor_thread = threading.Thread(target=self.executor.spin, daemon=True)
+        self.executor_thread.start()
         # Camera Setup
-        self.cam_one = GenCameraWidget(node_instance, 'Camera 1', '/rov/camera/image_raw')
-        self.cam_two = GenCameraWidget(node_instance, 'Camera 2', '/rov/camera/image_raw')
-        self.cam_three = GenCameraWidget(node_instance, 'Camera 3', '/rov/camera/image_raw')
+        self.cam_one = GenCameraWidget(self.node, 'Camera 1', '/rov/camera/image_raw')
+        self.cam_two = GenCameraWidget(self.node, 'Camera 2', '/rov/camera/image_raw')
+        self.cam_three = GenCameraWidget(self.node, 'Camera 3', '/rov/camera/image_raw')
         # Vertical Layout Setup
         self.small_cam_layout = QVBoxLayout()
         self.small_cam_layout.setContentsMargins(0, 0, 0, 0)
@@ -64,28 +69,28 @@ class GenCameraWidget(QWidget):
     # Consts
     PKG_PATH = get_package_share_directory('py_rov_gui')
     # Vars
-    image_signal = Signal(QPixmap)
+    image_signal = Signal(QImage)
     # Constructor
-    def __init__(self, node_instance, camera_name, sub_path):
+    def __init__(self, node_instance, camera_name, sub_topic):
         super().__init__()
         # Var
         self.is_subbed = False
-        # Setup
+        self.is_processing = False
+        self.callback_group = ReentrantCallbackGroup() # allows for parallel callbacks
         self.node = node_instance
-        self.setup_ui(sub_path, camera_name)
+        # setup
+        self.setup_ui(sub_topic, camera_name)
         # Connections
         self.image_signal.connect(self.update_image_label)
         self.bridge = CvBridge()
-        self.start_feed(sub_path)
-    # METHODSS
+        self.start_feed(sub_topic)
     # UI Setup Related Methods
-    def setup_ui(self, sub_path, camera_name):
+    def setup_ui(self, sub_topic, camera_name):
         # Labels
         # feed label setup
         self.feed = QLabel()
         self.feed.setScaledContents(True)
         self.feed.setObjectName('frame')
-        self.feed.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
         self.feed.setMinimumSize(1, 1)
         # name label setup
         self.name = QLabel(camera_name)
@@ -93,7 +98,7 @@ class GenCameraWidget(QWidget):
         self.name.setAlignment(Qt.AlignCenter)
         # Btn Setup
         self.buttons = {}
-        self.buttons['play'] = CustomButton(self, 'play_white.png', self.PKG_PATH, lambda: self.start_feed(sub_path), 50, 50)
+        self.buttons['play'] = CustomButton(self, 'play_white.png', self.PKG_PATH, lambda:self.start_feed(sub_topic), 50, 50)
         self.buttons['play'].setStyleSheet('background: green;')
         self.buttons['pause'] = CustomButton(self, 'pause_white.png', self.PKG_PATH, self.stop_feed, 50, 50)
         self.buttons['pause'].setStyleSheet('background: red;')
@@ -126,35 +131,35 @@ class GenCameraWidget(QWidget):
         else:
             self.feed.setPixmap(no_signal_pixmap)
     # Buttons / Feed Methods
-    def start_feed(self, sub_path): 
+    def start_feed(self, sub_topic): 
         if not self.is_subbed:
-            self.sub = self.node.create_subscription(Image, sub_path, self.callback, 10)
+            self.sub = self.node.create_subscription(CompressedImage, sub_topic, self.callback, 10, callback_group=self.callback_group)
             self.is_subbed = True
-            self.node.get_logger().info('Camera Feed Started...')
+            self.node.get_logger().info(f'{self.name.text()} Feed Started...')
     def stop_feed(self):
         if self.is_subbed:
             self.node.destroy_subscription(self.sub)
             self.sub = None
             self.is_subbed = False
-            self.node.get_logger().info('Camera Feed Stopped...')
-    # callback crap
+            self.node.get_logger().info(f'{self.name.text()} Feed Stopped...')
     def callback(self, msg):
+        if self.is_processing: # trying to remove any backlog of image processes occuring
+            return
         try:
-            frame = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-            rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            h, w, ch = rgb_image.shape
-            bytes_per_line = ch * w
-            qimg = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
-            pixmap = QPixmap.fromImage(qimg)
-            self.image_signal.emit(pixmap)
+            self.is_processing = True
+            # haveta convert from bgr to rgb, else pyqt will throw a fit and make everything colorblind
+            frame_bgr = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            frame_rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+            h, w, ch = frame_rgb.shape
+            qimg = QImage(frame_rgb.data, w, h, ch * w, QImage.Format_RGB888).copy()
+            self.image_signal.emit(qimg)
         except Exception as ex:
-            self.node.get_logger().error(f'ERROR in camera callback: {ex}')
-    # Slots
-    @Slot(QPixmap)
-    def update_image_label(self, pixmap):
-        scaled_pixmap = pixmap.scaled(self.feed.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        self.feed.setPixmap(scaled_pixmap)
-    # shutdown process
+            self.node.get_logger().error(f'ERROR in {self.name.text()} callback: {ex}')
+            self.is_processing = False
+    @Slot(QImage)
+    def update_image_label(self, qimg):
+        self.feed.setPixmap(QPixmap.fromImage(qimg))
+        self.is_processing = False
     def shutdown(self):
         if hasattr(self, 'sub') and self.sub:
             self.node.destroy_subscription(self.sub)
