ae05ea0a31ce23265fb5c20aad1484641d0a08fd
Tyerone <141754956+Pro5essorE@users.noreply.github.com>
2026-01-29 22:41:52 -0500

    publishes compressed img now

--- MROV-25-ROS-OPERATOR/simple_campub.py
+++ MROV-25-ROS-OPERATOR/simple_campub.py
@@ -1,89 +1,52 @@
 """
 Author: Tyerone Chen
-Last Update: 11/72025
+Last Update: 1/29/2026
 """
 # imports
 import rclpy
 import cv2
-import time
 from rclpy.node import Node
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge # Used for converting image types
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge
 from ament_index_python.packages import get_package_share_directory
-
 class CameraPublisher(Node):
-    # Vars
+    # Consts
     TOPIC_NAME = '/rov/camera/image_raw'
-    TIMER_INTERVAL = 0.0175
-    
+    TIMER_INTERVAL = 0.0167 # framerate is just 1/fps, so 1/60
     # Constructor
     def __init__(self):
         super().__init__('simple_campub')
-        """ for normal camera stuff?
-        self.pub = self.create_publisher(Image, self.TOPIC_NAME, 10)
-        self.timer = self.create_timer(self.TIMER_INTERVAL, self.timer_callback)
-        self.bridge = CvBridge() 
-        # init video capture
-        #self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2) # For actual camera
-        self.cap = cv2.VideoCapture('video-test-Goast_Bubber.mp4')
-        self.capture_failed = False
-        """
-        # For running mp4s or whatever videos
         package_name = 'sim_publisher'
         package_share_directory = get_package_share_directory(package_name)
         video_file_name = 'video-test-Goast_Bubber.mp4'
         video_path = f'{package_share_directory}/resource/{video_file_name}'
         self.get_logger().info(f"Attempting to open video at: {video_path}")
-        self.pub = self.create_publisher(Image, self.TOPIC_NAME, 10)
+        self.pub = self.create_publisher(CompressedImage, self.TOPIC_NAME, 10)
         self.timer = self.create_timer(self.TIMER_INTERVAL, self.timer_callback)
         self.bridge = CvBridge() 
         self.cap = cv2.VideoCapture(video_path) 
         self.capture_failed = False
-        # end of section
-        if not self.cap.isOpened(): # chedcker to see if this shit failed becausea sljdaljds
+        if not self.cap.isOpened():
             self.get_logger().error("Couldn't Open video Stream")
             self.capture_failed = True 
         else:
             self.get_logger().info("Publishing Node Started")
-            """ For Specifivally V4L2
-            time.sleep(0.5)
-            ret, frame = self.cap.read()
-            if not ret:
-                self.get_logger().warn("Initial camera read failed - setting capture_failed to True.")
-                self.capture_failed = True
-            """
     # callback waterver
     def timer_callback(self):
-        if self.capture_failed:
-            return
-        ret, frame = self.cap.read() 
+        ret, frame = self.cap.read()
         if not ret:
-            self.get_logger().info("Video stream ended, looping...")
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0) 
-            ret, frame = self.cap.read()
-            if not ret:
-                self.get_logger().warn("Failed to read frame after looping.")
-                return
-        if ret:
-            # Resolution
-            target_width = 640
-            target_height = 480
-            frame_resized = cv2.resize(frame, (target_width, target_height), interpolation=cv2.INTER_LINEAR)
-            img_msg = self.bridge.cv2_to_imgmsg(frame_resized, encoding="bgr8")
-            img_msg.header.stamp = self.get_clock().now().to_msg()
-            img_msg.header.frame_id = 'camera_frame'
-            self.pub.publish(img_msg) 
-
-    # shutdown thingy
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            return
+        frame_resized = cv2.resize(frame, (640, 480))
+        msg = self.bridge.cv2_to_compressed_imgmsg(frame_resized, dst_format='jpeg', )
+        self.pub.publish(msg)
     def shutdown(self):
         if self.cap.isOpened():
             self.cap.release()
         super().destroy_node()
-# main
 def main(args=None):
     rclpy.init(args=args)
     cam_pub = CameraPublisher()
-    
     try:
         rclpy.spin(cam_pub)
     except KeyboardInterrupt:
@@ -91,6 +54,5 @@ def main(args=None):
     finally:
         cam_pub.shutdown()
         rclpy.shutdown()
-
 if __name__ == '__main__':
     main()
