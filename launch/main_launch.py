92926d4911592386ade6a4b45f82d6c33928078d
alexw1230 <alexanderwiegand310@gmail.com>
2026-04-16 19:14:16 -0400

    fixed up launch, sim launch disabled rn

---
+++ launch/main_launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        # Joystick node
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node',
+            output='screen'
+        ),
+
+        # Controller publisher
+        Node(
+            package='MROV-25-ROS-OPERATOR',
+            executable='joy_to_controller',
+            name='joy_to_controller',
+            output='screen'
+        ),
+
+       
+    ])
